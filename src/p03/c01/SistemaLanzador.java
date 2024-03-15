1a45d6e0f88f5bfcb50625c7183b5d9321182cd9
juanpe316 <89323489+juanpe316@users.noreply.github.com>
2024-03-15 19:34:44 +0100

    Add files via upload
    Clase SistemaLanzador.

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,26 +1,39 @@
-package src.p03.c01;
+package Parque;
 
+/**
+ * Clase principal, la cual arranca la aplicación.
+ * 
+ * @author Juan Pedro Alarcón Gómez, Estíbalitz Díez Rioja.
+ * @since 1.0
+ * @version 1
+ */
 public class SistemaLanzador {
+
+	/**
+	 * Método main.
+	 * 
+	 * @param args Recibe argumentos del programa.
+	 */
 	public static void main(String[] args) {
-		
-		IParque parque = new Parque(); // TODO
+
+		IParque parque = new Parque();
 		char letra_puerta = 'A';
-		
+
 		System.out.println("¡Parque abierto!");
-		
+
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
+
+			String puerta = "" + ((char) (letra_puerta++));
+
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+			new Thread(entradas).start();
+
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread(salidas).start();
+
 		}
-	}	
+
+	}
 }
