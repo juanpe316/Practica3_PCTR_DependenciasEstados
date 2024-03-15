92ef4725d35fa8c4aa4b12af1562efc252148a64
juanpe316 <89323489+juanpe316@users.noreply.github.com>
2024-03-15 19:33:49 +0100

    Add files via upload
    Interfaz clase Parque.

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,9 +1,24 @@
-package src.p03.c01;
+package Parque;
 
+/**
+ * Interfaz Parque.
+ * 
+ * @author Juan Pedro Alarcón Gómez, Estíbalitz Díez Rioja.
+ * @since 1.0
+ * @version 1
+ */
 public interface IParque {
-	
+	/**
+	 * Entrada al parque.
+	 * 
+	 * @param puerta Por la que va a entrar.
+	 */
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
-
+	/**
+	 * Salida del parque.
+	 * 
+	 * @param idPuerta Por la que va a salir.
+	 */
+	public abstract void salirDelParque(String idPuerta);
 }
